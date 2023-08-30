4f00eccb8faea79bfe7411263720653fd04b9430
João Martins <jfbrancomartins@gmail.com>
2023-08-30 17:30:25 +0100

    deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route path='/' exact Component={Home} />
+        <Route path='/landing' exact Component={Home} />
       </Routes>
       <Footer/>
     </BrowserRouter>
